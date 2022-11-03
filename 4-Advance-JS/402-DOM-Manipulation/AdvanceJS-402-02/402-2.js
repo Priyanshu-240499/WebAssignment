9e4589c6c27e04159b99efd96249d112c4a8dcef
priyanshu <priyanshoochoudhary@gmail.com>
2022-11-03 04:03:15 -0700

    changes

--- 4-Advance-JS/402-DOM-Manipulation/AdvanceJS-402-02/402-2.js
+++ 4-Advance-JS/402-DOM-Manipulation/AdvanceJS-402-02/402-2.js
@@ -1,12 +1,12 @@
 var image =document.createElement("img");
 image.src="https://i0.wp.com/allthingstalent.org/wp-content/uploads/2022/07/geekster.jpg?fit=1000%2C535&ssl=1";
 var h2=document.createElement("h2");
-h2.innerText="Image Element Added."
+h2.innerText="Image Element Added.";
+document.getElementById("container").appendChild(h2);
 h2.style.display="none";
 function addImage(){
     if(h2.style.display=="none"){
-        h2.style.display="block";
-        document.getElementById("container").appendChild(h2);
+        h2.style.display="block";       
         document.getElementById("container").appendChild(image);
     }
     else{
